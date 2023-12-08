a6cb2370e25980cdc468700d2ae23b7c5a549be4
Jan Wartenberg <jan.wartenberg@web.de>
2023-12-08 23:31:28 +0100

    Try getify's version, tweaked 1st implementation, not nice but works for all examples; Add getify's test cases

--- speller.py
+++ speller.py
@@ -1,20 +1,19 @@
+import copy
 import json
+from pathlib import Path
 
 
 class PeriodicTable:
     # load JSON only once
-    with open("./static/periodic-table.json", "r") as f:
+    table_path = Path(__file__).parent / "static" / "periodic-table.json"
+    with open(table_path, "r") as f:
         elements = json.load(f)
     # the symbols are unique, so a dict is nicer than a pure list
-    elements = {el['symbol'].lower(): el for el in elements}
+    elements = {el["symbol"].lower(): el for el in elements}
 
     @staticmethod
     def lookup(symbol):
         return PeriodicTable.elements.get(symbol, {})
-
-    @staticmethod
-    def symbols():
-        return list(PeriodicTable.elements.keys())
 
 
 def spell_check(word):
@@ -22,24 +21,39 @@ def spell_check(word):
     with periodic table symbols;
     return array with them if so (empty array otherwise)"""
 
-    # naive: Iterate over the word and look for match
-    # TODO but: recursive/pathfinding approach might be needed,
-    #   if multiple matches exist
+    # Iterate over the word and look for match(es)
     possible_solution = []
+    ambiguity = []  # (index, possible_solution_then)
     i = 0
     while True:
         single = word[i]
-        double = word[i:i+2]
+        double = word[i : i + 2]
 
-        i += 1
-        if double in PeriodicTable.symbols():
+        if (
+            len(double) == 2
+            and double in PeriodicTable.elements
+            and single in PeriodicTable.elements
+        ):
+            #  if both cases possible, remember the state, go on with double
+            #  if we fail later, fall back to this state and continue with single
+            ambiguity.append((i, copy.copy(possible_solution)))
             i += 1
             possible_solution.append(double)
-        elif single in PeriodicTable.symbols():
+        elif double in PeriodicTable.elements:
+            i += 1
+            possible_solution.append(double)
+        elif single in PeriodicTable.elements:
             possible_solution.append(single)
         else:
-            break
+            # no match
+            if len(ambiguity) == 0:
+                break
+            else:
+                #  fall back if we had a different branch earlier
+                i, possible_solution = ambiguity.pop()
+                possible_solution.append(word[i])
 
+        i += 1
         if i >= len(word):
             break
 
@@ -48,6 +62,55 @@ def spell_check(word):
         return possible_solution
     else:
         return []
+
+    # def inner_check(word_part):
+    #   """ TODO trying recursive approach,
+    #    since multiple matches can exist - but this is still wrong """
+    #   # base case -> 1 character left
+    #     if len(word_part) == 1:
+    #         if word_part in PeriodicTable.elements:
+    #             return [word_part]
+    #         else:
+    #             return []
+    #
+    #     # TODO what about 2 characters left?
+    #     # how to branch?
+    #     if len(word_part) == 2:
+    #         if word_part in PeriodicTable.elements:
+    #             return [word_part]
+    #         # else: --> try it with further recursion
+    #
+    #     # recurse -> check smaller sub-string
+    #     #  but we need to append the results..
+    #     subpart = inner_check(word_part[:-1])
+    #     last = inner_check(word_part[-1])
+    #     if len(subpart) == 0 or len(last) == 0:
+    #         return []
+    #     return subpart + last
+
+    # def inner_check(word):
+    #     """ getify's first version """
+    #     if len(word) == 0:
+    #         return []
+    #     for symbol in PeriodicTable.elements:
+    #         if len(symbol) <= len(word):
+    #             # did it match the first or two chars?
+    #             if word[:len(symbol)] == symbol:
+    #                 # still chars left?
+    #                 if len(word) > len(symbol):
+    #                     res = inner_check(word[len(symbol):])
+    #                     # matched successfully?
+    #                     if len(res) > 0:
+    #                         return [symbol] + res
+    #                 else:
+    #                     return [symbol]
+    #     return []
+
+    # ret = inner_check(word)
+    # if "".join(ret) == word:
+    #     return ret
+    # else:
+    #     return []
 
 
 def check(word):
